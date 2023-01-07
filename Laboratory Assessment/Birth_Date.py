c32b0e9ace8b833a0c6309a875e4cc1975b8c862
Sid-0405 <103137013+Sid-0405@users.noreply.github.com>
2023-01-07 08:10:51 +0530

    Update Birth_Date.py

--- Laboratory Assessment/Birth_Date.py
+++ Laboratory Assessment/Birth_Date.py
@@ -1,58 +1,27 @@
-//created by Siddharth Kadu
-//prn:2030331245052
-from datetime import date
-import mysql.connector
-from email.message import EmailMessage, SEmailMessage 
+import datetime
+import pandas as pd
 import smtplib
-import medium as M
+import os
 
-today = date.today()
-db=mysql.connector.connect(host="localhost",user="root",passwd="xxxxxxxxxxxxxxx",database="test")
-cursor=db.cursor()
+os.chdir(r"D:\python practise\LAB")
 
-Holiday,Date = [],[]
-Email_ID,Month,Name,Number = [],[],[],[]
 
-def showData():
-    cursor.execute('SELECT * FROM Data')
-    data = cursor.fetchall()
-    for d in data:
-        print(d)
-
-def getData():
-    cursor.execute('SELECT * FROM Data')
-    data = cursor.fetchall()
-    for d in data:
-        Email_ID += [d[1]]
-        Month+=[(str(d[3])).split('-')]
-        Name += [d[0]]
-        Number += [d[2]]
-    
-def checkBirthday():
-    getData()
-    for i in range(len(Month)):
-        for j in range(len(Month[i])):
-            if Month[i][j]==str(today.month) and Month[i][j-1]==str(today.day):
-                print("Name of person's who have birthday today")
-                print(Name[i])
-                M.sendMail(Name[i],Email_ID[i]) 
-                M.sendWhatsApp(Name[i][j],Number[i]) 
-checkBirthday()
-
-msg = EmailMessage()
-Sender_Mail = "xxxxxxxxxxxxxxxx@gmail.com" 
-password = "xxxx xxxx xxxx xxxx" 
-
-def sendMail(Name,Reciver_Mail):
-    server = smtplib.SMTP('smtp.gmail.com',587) 
+def sendEmail(to, sub, msg):
+    server = smtplib.SMTP('smtp.gmail.com', 587);
     server.starttls()
-    server.login(Sender_Mail,password) 
-    msg['Subject'] = 'Testing of HAPPY BIRTHDAY Service!' 
-    msg['From'] = Sender_Mail
-    msg['To'] = Reciver_Mail
-    msg.set_content(f'Happiest Birthday {Name}! have a great day.')
-    server.send_message(msg)
-    server.quit()
+    server.login('siddharthkadu@gmail.com', "qkjwtopzuwysroij");
+    server.sendmail("siddharthkadu2001@gmail.com", to, f"Subject: {sub}\n\n{msg}");
+    server.quit();
 
-def sendWhatsApp(Name,Number):
-    body = f'Many Many Returns of the Day {Name}!\nI hope all your birthday wishes and dreams come true.' 
+
+if __name__ == "__main__":
+
+    df = pd.read_excel("data.xlsx")
+    today = datetime.datetime.now().strftime("%d-%m")
+    year = datetime.datetime.now().strftime("%Y")
+    # Excel Data
+    birthday="07/01"
+
+    if today == birthday:
+        print('siddharthkadu@gmail.com')
+        sendEmail('siddharthkadu2001@gmail.com', "Happy Birthday", 'Wish you a very happy birthday  🥳🎉🎊🎊')
